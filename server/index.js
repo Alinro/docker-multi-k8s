f3ca9b9f79d3bcd345f267a33cb4bb82d1d69485
Alin Dumitru <alin.dumitru20@gmail.com>
2024-04-16 13:41:29 +0200

    chore: some code updates

--- server/index.js
+++ server/index.js
@@ -16,14 +16,20 @@ const pgClient = new Pool({
     host: keys.pgHost,
     database: keys.pgDatabase,
     password: keys.pgPassword,
-    port: keys.pgPort
+    port: keys.pgPort,
+    ssl:
+        process.env.NODE_ENV !== 'production'
+        ? false
+        : { rejectUnauthorized: false },
 });
 
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch(err => console.log(err));
+pgClient.on("connect", (client) => {
+    client
+      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+      .catch((err) => console.error(err));
+  });
 
 // Redis Client Setup
 const redis = require('redis');
