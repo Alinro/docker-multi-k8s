fd8810c47f0b72577da55f51e20d6b5f41e67f41
Alin Dumitru <alin.dumitru20@gmail.com>
2024-04-17 16:10:17 +0200

    fix: added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <header>
-                    <div>
-                        <Link to="/">Home</Link>
-                    </div>
-                    <div>
-                        <Link to="/otherpage">Other page</Link>
-                    </div>
-                </header>
-                <div>
-                    <Route exact path="/" component={Fib} />
-                    <Route path="/otherpage" component={OtherPage} />
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Fib calculator</h1>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/otherpage">Other page</Link>
+          </div>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
